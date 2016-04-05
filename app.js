1300d8af7a41883cb46173d13e00e5d85244ca0c
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-04-05 17:34:56 -0500

    added env variables and botkit controller

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-var botkit = require('botkit');
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot();
+
+var bot = controller.spawn({
+  token: process.env.SLACK_ACCESS_TOKEN
+})
+
+bot.startRTM(function(err, bot, payload) {
+  if (err) {
+    throw new Error('Could not connect to Slack');
+  }
+});
