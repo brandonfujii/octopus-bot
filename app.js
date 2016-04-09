3763ad75fc87fc1c05dc46edbd3e0c65f06a84c9
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-04-08 20:06:10 -0500

    made the bot respond to hello

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+// use .env file
+require('dotenv').config()
 var Botkit = require('botkit');
 
+// Intialize botkit controller
 var controller = Botkit.slackbot();
 
+// Define a slack bot based on unique access token
 var bot = controller.spawn({
   token: process.env.SLACK_ACCESS_TOKEN
 })
 
+// Start bot and handle error if it fails
 bot.startRTM(function(err, bot, payload) {
   if (err) {
     throw new Error('Could not connect to Slack');
   }
+});
+
+controller.hears('hello', 'direct_message,direct_mention,mention', function(bot, message) {
+	bot.reply(message, 'Yoo!');
 });
