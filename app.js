6c968fd119f9bee66c79769bfcfa9d527d302a09
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-04-08 21:39:41 -0500

    added database and ability to add and show tasks

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // use .env file
 require('dotenv').config()
 var Botkit = require('botkit');
+var firebase_storage = require(__dirname + '/database/storage')({
+    firebase_uri: 'https://nuvention.firebaseio.com'
+});
+
+testObj0 = {id: 'TEST0', foo: 'bar0'};
+testObj1 = {id: 'TEST1', foo: 'bar1'};
 
 // Intialize botkit controller
 var controller = Botkit.slackbot();
@@ -17,6 +23,33 @@ bot.startRTM(function(err, bot, payload) {
   }
 });
 
-controller.hears('hello', 'direct_message,direct_mention,mention', function(bot, message) {
-	bot.reply(message, 'Yoo!');
+// Bot listens for 'add' to add a task to firebase
+controller.hears('add', 'direct_message,direct_mention,mention', function(bot, message) {
+	firebase_storage.teams.save(testObj1, function(err) {
+		if (err) {
+			bot.reply(message, 'Sorry, I couldn\'t add your task!');
+		}
+	})
 });
+
+// Bot listens for 'show tasks' to retrieve and display tasks from firebase
+controller.hears('show tasks', 'direct_message,direct_mention,mention', function(bot, message) {
+	firebase_storage.teams.all(function(err, data) {
+		if (err) {
+			bot.reply(message, 'Sorry, I couldn\'t retrieve tasks!');
+			return;
+		}
+
+		if (data) {
+
+			data.map(function(task) {
+				bot.reply(message, task.foo);
+			});
+
+		}
+
+	})
+
+});
+
+
