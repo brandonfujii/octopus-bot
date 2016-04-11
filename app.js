c97722bf812e963cad02cd7b26a4d42cfe5d3309
brandonfujii <brandonfujii2018@u.northwestern.edu>
2016-04-10 22:50:12 -0500

    added help documentation for slacktopus

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ bot.startRTM(function(err, bot, payload) {
 });
 
 // Task Object Constructor
-function Task(id, body, author) {
+function Task(id, body, author, assignee) {
 	this.id = id;
 	this.body = body;
 	this.author = author;
+	this.assignee = assignee;
 }
 
 // Parses command to get task user wants to interact with
@@ -38,12 +39,49 @@ function getTaskBody( str ){
 	}
 }
 
-// Bot listens for 'add' to add a task to firebase
+controller.hears('help', 'direct_message,direct_mention,mention', function(bot, message) {
+	var attachments = [];
+  var addTaskHelp = {
+  	title: 'Adding a task:',
+    color: '#FF7E82',
+    fields: [],
+  };
+
+  var showTasksHelp = {
+  	title: 'Viewing your team\'s tasks:',
+    color: '#FF7E82',
+    fields: [],
+  };
+
+  addTaskHelp.fields.push({
+    label: 'AddTask',
+    value: '@slacktopus: add (your_task)',
+    short: true,
+  });
+
+  showTasksHelp.fields.push({
+    label: 'ShowTask',
+    value: '@slacktopus: show tasks',
+    short: true,
+  });
+
+  attachments.push(addTaskHelp);
+  attachments.push(showTasksHelp)
+
+  bot.reply(message,{
+    text: 'Here are some commands you can perform with Octopus:',
+    attachments: attachments,
+  },function(err,resp) {
+    console.log(err,resp);
+  });
+});
+
+// ADD: Bot listens for 'add' to add a task to firebase
 controller.hears('add', 'direct_message,direct_mention,mention', function(bot, message) {
 	var command = message.text.split(" ")[0];
 	var body = getTaskBody(message.text);
 	var task_id = Date.now();
-	var task = new Task(task_id, body, message.user);
+	var task = new Task(task_id, body, message.user, null);
 
 	firebase_storage.teams.save(task, function(err) {
 		if (err) {
@@ -55,7 +93,19 @@ controller.hears('add', 'direct_message,direct_mention,mention', function(bot, m
 	})
 });
 
-// Bot listens for 'show tasks' to retrieve and display tasks from firebase
+// DELETE: 
+controller.hears('delete', 'direct_message,direct_mention,mention', function(bot, message) {
+	var command = message.text.split(" ")[0];
+	var task_id = parseInt(getTaskBody(message.text));
+
+	firebase_storage.teams.get(task_id, function(err, team) {
+
+	})
+
+});
+
+
+// SHOW TASKS: Bot listens for 'show tasks' to retrieve and display tasks from firebase
 controller.hears('show tasks', 'direct_message,direct_mention,mention', function(bot, message) {
 	firebase_storage.teams.all(function(err, data) {
 		if (err) {
@@ -66,7 +116,8 @@ controller.hears('show tasks', 'direct_message,direct_mention,mention', function
 		if (data) {
 
 			data.map(function(task) {
-				bot.reply(message, task.foo);
+				var list_item = "Task " + task.id + ": " + task.body;
+				bot.reply(message, list_item);
 			});
 
 		}
@@ -74,5 +125,23 @@ controller.hears('show tasks', 'direct_message,direct_mention,mention', function
 	})
 
 });
+
+// CLAIM: Bot listens for 'claim' to have the user claim a task
+controller.hears('claim', 'direct_message,direct_mention,mention', function(bot, message) {
+	var command = message.text.split(" ")[0];
+	var task_id = getTaskBody(message.text);
+
+	//TODO: assign task to user
+
+	firebase_storage.teams.get(task_id, function(err, team) {
+		if (err) {
+			bot.reply(message, 'Sorry, I couldn\'t add your task!');
+		}
+		else {
+			bot.reply(message, "[" + team.body + "] has been claimed.");
+		}
+	})
+});
+
 
 
